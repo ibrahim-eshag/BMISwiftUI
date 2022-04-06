ce9203b2d5d73aab30f62b8cadc6ca11388e0cdf
Amged <hemaisne@yahoo.com>
2022-04-06 12:47:21 +0200

    Initial Commit

---
+++ BMISwiftUI/BMISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMISwiftUIApp.swift
+//  BMISwiftUI
+//
+//  Created by Amged on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BMISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
